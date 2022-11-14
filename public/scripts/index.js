4df8c581aadffc1ed2cebb0398cebec11f3c3d04
Sidhesh <sidheshlawangare@gmail.com>
2022-11-14 16:36:06 +0530

    Initial

---
+++ public/scripts/index.js
@@ -0,0 +1,39 @@
+var h=document.getElementById('down')
+var s=document.getElementsByClassName('like')[0]
+h.onclick=()=>{
+    s.classList.toggle('like')
+    }
+z=document.getElementsByClassName('ind')
+Array.from(z).forEach(element => {
+    element.onclick=()=>{
+        newj.innerHTML=element.innerHTML
+    }
+});
+var k=document.getElementById('im2')
+var j=document.getElementsByClassName('luke')[0]
+k.onclick=()=>{
+    j.classList.toggle('luke')
+    }
+v=document.getElementsByClassName('jiu')
+Array.from(z).forEach(element => {
+    element.onclick=()=>{
+        newj.innerHTML=element.innerHTML
+    }
+});
+var t=document.getElementsByClassName('jiu');
+t.onclick=()=>{
+     t.style.color='yellow'
+}
+var u=document.getElementById('arrow')
+var v=document.getElementById('arrow2')
+var o=document.getElementsByClassName('im2')[0]
+u.onclick=()=>{
+    o.classList.toggle('im2')
+}
+v.onclick=()=>{
+    o.classList.toggle('im2')
+}
+
+
+
+
